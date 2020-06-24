761b71dca17e35dfa0a3adc14b6e53e4d8e4a95f
Ilpo Loikkanen <ilpo.loikkanen@haksi.fi>
2020-06-24 08:37:41 +0000

    read raw file

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+
 import express from 'express'
+import fs from 'fs'
+const app = express()
+const port = 5000
 
-const api = express();
+app.get('/', (req, res) => res.send(''))
 
-api.get(('/raw', (req, res) => {
-    var fs = require('fs');
-    let raw = fs.readFileSync('raw.txt','utf-8');
+app.get('/raw', (req, res) => {
+    console.log("read raw")
+    var raw = readFile("./raw.txt");
     return res.send(raw);
 });
+
+function readFile(f) {
+    return fs.readFileSync('./raw.txt','utf-8');
+}
+
+app.listen(port, () => console.log(`port: ${port}`))
