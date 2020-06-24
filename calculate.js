b5574ee055f570eb7d0426b4c8bd97054d8ec763
Ilpo Loikkanen <ilpo.loikkanen@haksi.fi>
2020-06-24 14:03:44 +0000

    BCD and LONG calculations

--- calculate.js
+++ calculate.js
@@ -2,22 +2,36 @@ export default function convertTable(arr) {
 
     //combine real4 hex strings and convert to float
     arr.map((obj, i) => {
-        if(obj[0] =="REAL4") {
-            if(obj[2] !== 0) {
-            if(arr[i+1][3] === obj[3]) {
-                //combine hex strings
-                let t = obj[2].toString().concat(arr[i+1][2].toString())
-                obj[2] = real4toFloat(t);
-                obj = arr[i]
-                //delete duplicates
-                arr.splice(i+1,1);
-            }
+        if(obj[2] !== 0) {
+        switch(obj[0]){
+            case "REAL4": 
+                if(arr[i+1][3] === obj[3]) {
+                    //combine hex strings
+                    let t = obj[2].toString().concat(arr[i+1][2].toString())
+                    obj[2] = real4toFloat(t);
+                    obj = arr[i]
+                    //delete duplicates
+                    arr.splice(i+1,1);
+                }
+                break;
+            case "BCD":
+                BCDtoNumber(obj[2])
+                break;
+            case "LONG":
+                //hex ffff ffc8 = -56 sign 32bit int
+                if(arr[i+1][3] === obj[3]) {
+                    let t = arr[i+1][2].toString().concat(obj[2].toString());
+                    // num|0 to force 32bit int
+                    obj[2] = parseInt(t,16)|0;
+                    obj = arr[i]
+                    arr.splice(i+1,1);
+                }
+                break;
         }
-        }
-    })
+    }
+    });
     return arr
 }
-
 
 // help from stack overflow for converting real4 to float value https://stackoverflow.com/questions/59854184/hex-to-float-mid-little-endian-cdab-hex-to-uint32-big-endian-abcd
 function real4toFloat(str) {
@@ -32,3 +46,14 @@ function real4toFloat(str) {
     return new DataView(reordered.buffer).getFloat32(0)
 }
 
+function BCDtoNumber(bcd) 
+{
+    var n = 0;
+    var m = 1;
+    for(var i = 0; i<bcd.length; i+=1) {
+        n += (bcd[bcd.length-1-i] & 0x0F) * m;
+        n += ((bcd[bcd.length-1-i]>>4) & 0x0F) * m * 10;
+        m *= 100;
+    }
+    return n;
+}
